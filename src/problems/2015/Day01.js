ebfd4a39f05182c697ffdfdca1abcbde2e494b00
Barney Boisvert <>
2018-11-18 14:48:48 -0800

    implement 2015 Day 1

--- src/problems/2015/Day01.js
+++ src/problems/2015/Day01.js
@@ -2,7 +2,25 @@ const Day00 = {
     event: "2015",
     day: 1,
     title: "Not Quite Lisp",
-    partOne: input => input.trim().length,
+    partOne: input => {
+        input = input.trim();
+        let floor = 0;
+        for (var i = 0, l = input.length; i < l; i++) {
+            floor += (input.charAt(i) === "(" ? 1 : -1);
+        }
+        return floor;
+    },
+    partTwo: input => {
+        input = input.trim();
+        let floor = 0;
+        for (var i = 0, l = input.length; i < l; i++) {
+            floor += (input.charAt(i) === "(" ? 1 : -1);
+            if (floor < 0) {
+                return i + 1;
+            }
+        }
+        return -1;
+    },
 };
 
 export default Day00;
