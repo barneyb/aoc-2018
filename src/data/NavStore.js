9261d7cadfcb55caecf58982d5bf06ca558de542
Barney Boisvert <>
2018-11-18 14:55:47 -0800

    track the active problem through localStorage

--- src/data/NavStore.js
+++ src/data/NavStore.js
@@ -2,25 +2,38 @@ import {ReduceStore} from 'flux/utils';
 import Actions from "./Actions";
 import Dispatcher from "./Dispatcher";
 
+const ACTIVE_PROBLEM_LOCAL_STORAGE_KEY = "aoc-active-problem";
+
 class NavStore extends ReduceStore {
     constructor() {
         super(Dispatcher);
     }
 
     getInitialState() {
+        const v = window.localStorage.getItem(ACTIVE_PROBLEM_LOCAL_STORAGE_KEY);
+        let currProb = null;
+        if (v != null && v.indexOf("/") > 0) {
+            const parts = v.split("/");
+            currProb = {
+                event: parts[0],
+                day: parseInt(parts[1]),
+            }
+        }
         return {
-            currentProblem: null,
+            currentProblem: currProb,
         };
     }
 
     reduce(state, action) {
         switch (action.type) {
             case Actions.GO_HOME:
+                window.localStorage.removeItem(ACTIVE_PROBLEM_LOCAL_STORAGE_KEY);
                 return {
                     ...state,
                     currentProblem: null,
                 };
             case Actions.SELECT_PROBLEM:
+                window.localStorage.setItem(ACTIVE_PROBLEM_LOCAL_STORAGE_KEY, action.event + "/" + action.day);
                 return {
                     ...state,
                     currentProblem: {
