e68c5f55618dc63836b959891e7d9cba2562e934
Barney Boisvert <bboisvert@gmail.com>
2018-12-01 16:55:43 -0800

    create an `ST` interface, based on the _Algorithms_ book (pg 363)

---
+++ api/src/main/java/com/barneyb/aoc2018/api/ST.java
@@ -0,0 +1,28 @@
+package com.barneyb.aoc2018.api;
+
+// taken from Algorithms, pg 363
+public interface ST<K, V> {
+
+    // ST();
+
+    void put(K key, V val);
+
+    V get(K key);
+
+    default void delete(K key) {
+        put(key, null);
+    }
+
+    default boolean contains(K key) {
+        return get(key) != null;
+    }
+
+    default boolean isEmpty() {
+        return size() == 0;
+    }
+
+    int size();
+
+    // Iterable<Keys> keys()
+
+}
