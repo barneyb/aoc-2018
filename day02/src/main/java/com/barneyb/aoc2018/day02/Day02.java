16e274d340070d0d5de6742ee052a80e5c3cbe1c
Barney Boisvert <bboisvert@gmail.com>
2018-12-02 10:22:16 -0800

    Day 2 part one

--- day02/src/main/java/com/barneyb/aoc2018/day02/Day02.java
+++ day02/src/main/java/com/barneyb/aoc2018/day02/Day02.java
@@ -8,10 +8,49 @@ public class Day02 extends OneShotDay {
 
     @Override
     public Answers solve(String input) {
+        String[] ids = parse(input);
         return new Answers(
-                input.length(),
-                input.trim().length()
+                getPartOne(ids)
         );
+    }
+
+    int getPartOne(String[] ids) {
+        Histogram<Character>[] hs = buildFrequencies(ids);
+        int two = countWithFrequency(hs, 2);
+        int three = countWithFrequency(hs, 3);
+        return two * three;
+    }
+
+    private int countWithFrequency(Histogram<Character>[] hs, int i) {
+        int n = 0;
+        for (Histogram<Character> h : hs) {
+            if (anyLetterWithFrequency(h, i)) {
+                n += 1;
+            }
+        }
+        return n;
+    }
+
+    private boolean anyLetterWithFrequency(Histogram<Character> h, int i) {
+        for (Character c : h.keys()) {
+            if (h.get(c) == i) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String[] parse(String input) {
+        return input.trim().split("\n");
+    }
+
+    Histogram<Character>[] buildFrequencies(String[] strings) {
+        @SuppressWarnings("unchecked")
+        Histogram<Character>[] hs = new Histogram[strings.length];
+        for (int i = 0, l = strings.length; i < l; i++) {
+            hs[i] = Histogram.fromString(strings[i]);
+        }
+        return hs;
     }
 
     public static void main(String[] args)  {
--- day02/src/test/java/com/barneyb/aoc2018/day02/Day02Test.java
+++ day02/src/test/java/com/barneyb/aoc2018/day02/Day02Test.java
@@ -1,18 +1,26 @@
 package com.barneyb.aoc2018.day02;
 
-import com.barneyb.aoc2018.api.Day;
-import com.barneyb.aoc2018.api.impl.Answers;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Day02Test {
 
+    private static String[] EXAMPLE_1 = {
+            "abcdef",
+            "bababc",
+            "abbcde",
+            "abcccd",
+            "aabcdd",
+            "abcdee",
+            "ababab",
+    };
+
     @Test
-    public void solve() {
-        Answers a = new Day02().solve("  cat  ");
-        assertEquals("7", a.getPartOne());
-        assertEquals("3", a.getPartTwo());
+    public void partOne() {
+        assertEquals(12, new Day02().getPartOne(EXAMPLE_1));
     }
+
+
 
 }
