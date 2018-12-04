6e56633723d60da622887b00408675c764296ecc
Barney Boisvert <bboisvert@gmail.com>
2018-12-04 11:33:19 -0800

    allow stopping a Stopwatch, in case the elapsed time is needed later

--- utils/src/main/java/com/barneyb/aoc2018/util/Stopwatch.java
+++ utils/src/main/java/com/barneyb/aoc2018/util/Stopwatch.java
@@ -3,9 +3,15 @@ package com.barneyb.aoc2018.util;
 public class Stopwatch {
 
     private final long start = System.currentTimeMillis();
+    private long stop = 0;
+
+    public long stop() {
+        stop = System.currentTimeMillis();
+        return elapsed();
+    }
 
     public long elapsed() {
-        return System.currentTimeMillis() - start;
+        return (stop == 0 ? System.currentTimeMillis() : stop) - start;
     }
 
 }
