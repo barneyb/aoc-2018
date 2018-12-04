22cf5d98f64f7d26f0a92eb6af13846007b3316d
Barney Boisvert <bboisvert@gmail.com>
2018-12-04 14:42:28 -0800

    Day 4 part one

--- day04/src/main/java/com/barneyb/aoc2018/day04/Day04.java
+++ day04/src/main/java/com/barneyb/aoc2018/day04/Day04.java
@@ -1,17 +1,67 @@
 package com.barneyb.aoc2018.day04;
 
-import com.barneyb.aoc2018.util.Answers;
-import com.barneyb.aoc2018.util.FileUtils;
-import com.barneyb.aoc2018.util.OneShotDay;
+import com.barneyb.aoc2018.util.*;
 
 public class Day04 extends OneShotDay {
 
     @Override
     public Answers solve(String input) {
+        Record[] rs = parse(input);
         return new Answers(
-                input.length(),
-                input.trim().length()
+                partOne(rs)
         );
+    }
+
+    private int partOne(Record[] rs) {
+        Queue<Nap> naps = getNaps(rs);
+        Integer id = sleepiestGuard(naps);
+        Integer minute = sleepiestMinute(naps, id);
+        return id * minute;
+    }
+
+    static Integer sleepiestMinute(Queue<Nap> naps, Integer guardId) {
+        Histogram<Integer> minutes = new Histogram<>();
+        for (Nap n : naps) {
+            if (n.getGuardId() == guardId) {
+                for (int i = n.getStart(), l = n.getEnd(); i < l; i++) {
+                    minutes.count(i);
+                }
+            }
+        }
+        return minutes.mostFrequent();
+    }
+
+    static Integer sleepiestGuard(Queue<Nap> naps) {
+        Histogram<Integer> guards = new Histogram<>();
+        for (Nap n : naps) {
+            guards.add(n.getGuardId(), n.getLength());
+        }
+        return guards.mostFrequent();
+    }
+
+    static Queue<Nap> getNaps(Record[] rs) {
+        Queue<Nap> naps = new Queue<>();
+        Timestamp asleepAt = null;
+        Integer thisGuard = null;
+        for (Record r : rs) {
+            switch (r.getAction()) {
+                case BEGIN:
+                    thisGuard = r.getGuardId();
+                    break;
+                case FALL_ASLEEP:
+                    asleepAt = r.getTs();
+                    break;
+                case WAKE_UP:
+                    //noinspection ConstantConditions
+                    naps.enqueue(new Nap(
+                            thisGuard,
+                            asleepAt.getMinute(),
+                            r.getTs().getMinute()
+                    ));
+                    break;
+            }
+        }
+        return naps;
     }
 
     public static Record[] parse(String input) {
@@ -20,14 +70,25 @@ public class Day04 extends OneShotDay {
         for (int i = 0, l = lines.length; i < l; i++) {
             rs[i] = Record.fromString(lines[i]);
         }
+        Sort.sort(rs);
         return rs;
     }
 
     public static void main(String[] args)  {
         Day04 d = new Day04();
         String input = FileUtils.readFile("day04/input.txt");
-        Answers a = d.solve(input);
-        System.out.println(a);
+        Record[] rs = parse(input);
+        System.out.println("Before:");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(rs[i]);
+        }
+        Stopwatch w = new Stopwatch();
+        Sort.sort(rs);
+        w.stop();
+        System.out.println("After (" + w.elapsed() + " ms):");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(rs[i]);
+        }
     }
 
 }
--- day04/src/test/java/com/barneyb/aoc2018/day04/Day04Test.java
+++ day04/src/test/java/com/barneyb/aoc2018/day04/Day04Test.java
@@ -1,8 +1,10 @@
 package com.barneyb.aoc2018.day04;
 
 import com.barneyb.aoc2018.util.Answers;
+import com.barneyb.aoc2018.util.Queue;
 import org.junit.Test;
 
+import static java.lang.Integer.valueOf;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
@@ -16,6 +18,18 @@ public class Day04Test {
     public static final String GUARD_99 = "[1518-11-01 23:58] Guard #99 begins shift\n" +
             "[1518-11-02 00:40] falls asleep\n" +
             "[1518-11-02 00:50] wakes up\n";
+    public static final Record[] RECORDS_10 = {
+            new Record(new Timestamp(1518, 11, 1, 0, 0), Action.BEGIN, 10),
+            new Record(new Timestamp(1518, 11, 1, 0, 5), Action.FALL_ASLEEP),
+            new Record(new Timestamp(1518, 11, 1, 0, 25), Action.WAKE_UP),
+            new Record(new Timestamp(1518, 11, 1, 0, 30), Action.FALL_ASLEEP),
+            new Record(new Timestamp(1518, 11, 1, 0, 55), Action.WAKE_UP),
+    };
+    public static final Record[] RECORDS_99 = {
+            new Record(new Timestamp(1518, 11, 1, 23, 58), Action.BEGIN, 99),
+            new Record(new Timestamp(1518, 11, 2, 0, 40), Action.FALL_ASLEEP),
+            new Record(new Timestamp(1518, 11, 2, 0, 50), Action.WAKE_UP),
+    };
     private static String EXAMPLE_INPUT = GUARD_10 +
             GUARD_99 +
             "[1518-11-03 00:05] Guard #10 begins shift\n" +
@@ -37,18 +51,22 @@ public class Day04Test {
 
     @Test
     public void parse() {
-        assertArrayEquals(new Record[] {
-                new Record(new Timestamp(1518, 11, 1, 0, 0), Action.BEGIN, 10),
-                new Record(new Timestamp(1518, 11, 1, 0, 5), Action.FALL_ASLEEP),
-                new Record(new Timestamp(1518, 11, 1, 0, 25), Action.WAKE_UP),
-                new Record(new Timestamp(1518, 11, 1, 0, 30), Action.FALL_ASLEEP),
-                new Record(new Timestamp(1518, 11, 1, 0, 55), Action.WAKE_UP),
-        }, Day04.parse(GUARD_10));
+        assertArrayEquals(RECORDS_10, Day04.parse(GUARD_10));
+        assertArrayEquals(RECORDS_99, Day04.parse(GUARD_99));
+    }
 
-        assertArrayEquals(new Record[] {
-                new Record(new Timestamp(1518, 11, 1, 23, 58), Action.BEGIN, 99),
-                new Record(new Timestamp(1518, 11, 2, 0, 40), Action.FALL_ASLEEP),
-                new Record(new Timestamp(1518, 11, 2, 0, 50), Action.WAKE_UP),
-        }, Day04.parse(GUARD_99));
+    @Test
+    public void sleepiestGuard() {
+        Record[] rs = Day04.parse(EXAMPLE_INPUT);
+        Queue<Nap> naps = Day04.getNaps(rs);
+        assertEquals(valueOf(10), Day04.sleepiestGuard(naps));
+    }
+
+    @Test
+    public void getNaps() {
+        assertEquals(new Queue<>(new Nap[] {
+                new Nap(10, 5, 25),
+                new Nap(10, 30, 55),
+        }), Day04.getNaps(RECORDS_10));
     }
 }
