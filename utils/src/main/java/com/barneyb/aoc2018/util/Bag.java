f16c1bf79e92449e833f99a8a27a097ad6899e9d
Barney Boisvert <bboisvert@gmail.com>
2018-12-07 10:10:33 -0800

    Bag type, again built atop Dequeue

---
+++ utils/src/main/java/com/barneyb/aoc2018/util/Bag.java
@@ -0,0 +1,55 @@
+package com.barneyb.aoc2018.util;
+
+import java.util.Iterator;
+
+public class Bag<E> implements Iterable<E> {
+
+    private final Deque<E> deque = new Deque<>();
+
+    public Bag() {}
+
+    public Bag(E[] elements) {
+        for (E e : elements) {
+            add(e);
+        }
+    }
+
+    public void add(E element) {
+        deque.push(element);
+    }
+
+    public int size() {
+        return deque.size();
+    }
+
+    public boolean isEmpty() {
+        return deque.isEmpty();
+    }
+
+    public void clear() {
+        deque.clear();
+    }
+
+    public Iterator<E> iterator() {
+        return deque.iterator();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bag)) return false;
+        Bag<?> bag = (Bag<?>) o;
+        return deque.equals(bag.deque);
+    }
+
+    @Override
+    public int hashCode() {
+        return deque.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return deque.toString();
+    }
+
+}
