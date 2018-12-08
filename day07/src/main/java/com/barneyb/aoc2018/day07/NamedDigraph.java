46977965b036a5e9d6c95871a129bae23a054f70
Barney Boisvert <bboisvert@gmail.com>
2018-12-07 16:54:57 -0800

    pull AbstractEdge for reuse

--- day07/src/main/java/com/barneyb/aoc2018/day07/NamedDigraph.java
+++ day07/src/main/java/com/barneyb/aoc2018/day07/NamedDigraph.java
@@ -7,8 +7,47 @@ import com.barneyb.aoc2018.util.TreeSet;
 public class NamedDigraph<Name extends Comparable<Name>> {
 
     public interface Edge<T> {
-        T getSource();
-        T getTarget();
+        T source();
+        T target();
+    }
+
+    public static abstract class AbstractEdge<T extends Comparable<T>> implements Edge<T> {
+        final T source;
+        final T target;
+
+        AbstractEdge(T source, T target) {
+            this.source = source;
+            this.target = target;
+        }
+
+        @Override
+        public T source() {
+            return source;
+        }
+
+        @Override
+        public T target() {
+            return target;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof AbstractEdge)) return false;
+
+            //noinspection unchecked
+            AbstractEdge edge = (AbstractEdge) o;
+
+            if (!source.equals(edge.source)) return false;
+            return this.target.equals(edge.target);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = source.hashCode();
+            result = 31 * result + target.hashCode();
+            return result;
+        }
     }
 
     private final Name[] toName;
@@ -18,8 +57,8 @@ public class NamedDigraph<Name extends Comparable<Name>> {
     public NamedDigraph(Edge<Name>[] ps) {
         TreeSet<Name> names = new TreeSet<>();
         for (Edge<Name> p : ps) {
-            names.add(p.getSource());
-            names.add(p.getTarget());
+            names.add(p.source());
+            names.add(p.target());
         }
         //noinspection unchecked
         toName = (Name[]) new Comparable[names.size()];
@@ -32,7 +71,7 @@ public class NamedDigraph<Name extends Comparable<Name>> {
         }
         g = new Digraph(names.size());
         for (Edge<Name> p : ps) {
-            g.addEdge(getSite(p.getSource()), getSite(p.getTarget()));
+            g.addEdge(getSite(p.source()), getSite(p.target()));
         }
     }
 
--- day07/src/main/java/com/barneyb/aoc2018/day07/Prerequisite.java
+++ day07/src/main/java/com/barneyb/aoc2018/day07/Prerequisite.java
@@ -1,41 +1,9 @@
 package com.barneyb.aoc2018.day07;
 
-public class Prerequisite implements NamedDigraph.Edge<String> {
-
-    final String source;
-    final String target;
+public class Prerequisite extends NamedDigraph.AbstractEdge<String> {
 
     Prerequisite(String source, String target) {
-        this.source = source;
-        this.target = target;
-    }
-
-    @Override
-    public String getSource() {
-        return source;
-    }
-
-    @Override
-    public String getTarget() {
-        return target;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Prerequisite)) return false;
-
-        Prerequisite prereq = (Prerequisite) o;
-
-        if (!source.equals(prereq.source)) return false;
-        return this.target.equals(prereq.target);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = source.hashCode();
-        result = 31 * result + target.hashCode();
-        return result;
+        super(source, target);
     }
 
     @Override
