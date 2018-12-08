1604cd3eaa3baaf8249553aaeade0a4d2a9005f1
Barney Boisvert <bboisvert@gmail.com>
2018-12-08 10:08:40 -0800

    reimplement Tree w/ actual parsing, not just scanning

--- day08/src/main/java/com/barneyb/aoc2018/day08/Tree.java
+++ day08/src/main/java/com/barneyb/aoc2018/day08/Tree.java
@@ -1,28 +1,76 @@
 package com.barneyb.aoc2018.day08;
 
+import com.barneyb.aoc2018.util.Bag;
+
+import java.util.Iterator;
+
 public class Tree {
 
-    private int metaSum;
+    private static class Node {
+        Bag<Node> children = new Bag<>();
+        Bag<Integer> metadata = new Bag<>();
 
-    Tree(int[] ns) {
-        doNode(ns, 0);
+        void addChild(Node n) {
+            children.add(n);
+        }
+
+        void addMeta(int n) {
+            metadata.add(n);
+        }
+
+        int getMetaSum() {
+            int sum = 0;
+            for (Node c : children) {
+                sum += c.getMetaSum();
+            }
+            for (int n : metadata) {
+                sum += n;
+            }
+            return sum;
+        }
     }
 
-    // do the node at i, recurse, and return the index after the node
-    private int doNode(int[] ns, int i) {
-        int childCount = ns[i++];
-        int metaCount = ns[i++];
+    private static class Digest implements Iterator<Integer> {
+        private final int[] ns;
+        private int i;
+
+        public Digest(int[] ns, int i) {
+            this.ns = ns;
+            this.i = i;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return i < ns.length;
+        }
+
+        @Override
+        public Integer next() {
+            return ns[i++];
+        }
+    }
+
+    private Node root;
+
+    Tree(int[] ns) {
+        this.root = readNode(new Digest(ns, 0));
+    }
+
+    private Node readNode(Digest d) {
+        Node n = new Node();
+        int childCount = d.next();
+        int metaCount = d.next();
         // eat the children
         for (int j = 0; j < childCount; j++) {
-            i = doNode(ns, i);
+            n.addChild(readNode(d));
         }
         for (int j = 0; j < metaCount; j++) {
-            metaSum += ns[i++];
+            n.addMeta(d.next());
         }
-        return i;
+        return n;
     }
 
     public int getMetaSum() {
-        return metaSum;
+        return root.getMetaSum();
     }
 }
