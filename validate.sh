9141951c6e84a738fa7b00acfc43bfd43e79bb36
Barney Boisvert <bboisvert@gmail.com>
2018-12-07 17:53:12 -0800

    validate script that'll check all solvers against cached results

---
+++ validate.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+cd `dirname $0`
+
+if [ `git status --porcelain | wc -l` != "0" ]; then
+    echo "Your working copy is dirty; I refuse"
+    exit 1
+fi
+
+expected=runner/target/expected.log
+actual=runner/target/actual.log
+
+mvn clean package
+cat ./completion.log | egrep -v '^[0-9]+ ms' > ${expected}
+./solve.sh --all | egrep -v '^[0-9]+ ms' > ${actual}
+
+diff ${expected} ${actual}
