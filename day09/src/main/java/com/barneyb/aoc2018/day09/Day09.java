f79a1adb13b9ee045757564e425097f551da5385
Barney Boisvert <bboisvert@gmail.com>
2018-12-09 09:01:48 -0800

    skeleton for day 9

---
+++ day09/src/main/java/com/barneyb/aoc2018/day09/Day09.java
@@ -0,0 +1,24 @@
+package com.barneyb.aoc2018.day09;
+
+import com.barneyb.aoc2018.util.Answers;
+import com.barneyb.aoc2018.util.OneShotDay;
+import com.barneyb.aoc2018.util.FileUtils;
+
+public class Day09 extends OneShotDay {
+
+    @Override
+    public Answers solve(String input) {
+        return new Answers(
+                input.length(),
+                input.trim().length()
+        );
+    }
+
+    public static void main(String[] args)  {
+        Day09 d = new Day09();
+        String input = FileUtils.readFile("day09/input.txt");
+        Answers a = d.solve(input);
+        System.out.println(a);
+    }
+
+}
