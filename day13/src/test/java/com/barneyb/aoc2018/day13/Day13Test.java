03fc52c3a7a0b034a5c5416066e63e424f503ad7
Barney Boisvert <bboisvert@gmail.com>
2018-12-13 13:48:49 -0800

    ditch the text files - whitespace is annoying

--- day13/src/test/java/com/barneyb/aoc2018/day13/Day13Test.java
+++ day13/src/test/java/com/barneyb/aoc2018/day13/Day13Test.java
@@ -1,36 +1,34 @@
 package com.barneyb.aoc2018.day13;
 
 import com.barneyb.aoc2018.util.Answers;
-import com.barneyb.aoc2018.util.Point;
-import com.barneyb.aoc2018.util.Resources;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class Day13Test {
 
-    static final String EXAMPLE_INPUT = Resources.asText("example.txt");
-
-    static final Cart[] EXAMPLE_CARTS = {
-            new Cart('A', new Point(2, 0), '-', Dir.EAST),
-            new Cart('B', new Point(9, 3), '|', Dir.SOUTH)
-    };
-
-    static final String[] EXAMPLE_GRID = {
-            // this looks janky cuzza escapes
-            "/-A-\\",
-            "|   |  /----\\",
-            "| /-+--+-\\  |",
-            "| | |  | B  |",
-            "\\-+-/  \\-+--/",
-            "  \\------/"
-    };
+    static final String EXAMPLE_INPUT =
+            "/->-\\        \n" +
+            "|   |  /----\\\n" +
+            "| /-+--+-\\  |\n" +
+            "| | |  | v  |\n" +
+            "\\-+-/  \\-+--/\n" +
+            "  \\------/   ";
+    static final String EXAMPLE_TWO_INPUT =
+            "/>-<\\  \n" +
+            "|   |  \n" +
+            "| /<+-\\\n" +
+            "| | | v\n" +
+            "\\>+</ |\n" +
+            "  |   ^\n" +
+            "  \\<->/";
 
     @Test
     public void solve() {
         Answers a = new Day13().solve(EXAMPLE_INPUT);
         assertEquals("7,3", a.getPartOne());
-        assertEquals("-", a.getPartTwo());
+        a = new Day13().solve(EXAMPLE_TWO_INPUT);
+        assertEquals("6,4", a.getPartTwo());
     }
 
 }
--- day13/src/test/java/com/barneyb/aoc2018/day13/MapTest.java
+++ day13/src/test/java/com/barneyb/aoc2018/day13/MapTest.java
@@ -11,21 +11,21 @@ public class MapTest {
         Map m = Map.parse(Day13Test.EXAMPLE_INPUT);
         assertEquals(
                 // this looks janky cuzza escapes
-                "/---\\\n" +
-                        "|   |  /----\\\n" +
-                        "| /-+--+-\\  |\n" +
-                        "| | |  | |  |\n" +
-                        "\\-+-/  \\-+--/\n" +
-                        "  \\------/",
+                "/---\\        \n" +
+                "|   |  /----\\\n" +
+                "| /-+--+-\\  |\n" +
+                "| | |  | |  |\n" +
+                "\\-+-/  \\-+--/\n" +
+                "  \\------/   ",
                 m.toString(false));
         assertEquals(
                 // this looks janky cuzza escapes
-                "/-A-\\\n" +
+                "/-A-\\        \n" +
                 "|   |  /----\\\n" +
                 "| /-+--+-\\  |\n" +
                 "| | |  | B  |\n" +
                 "\\-+-/  \\-+--/\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
     }
 
@@ -34,57 +34,72 @@ public class MapTest {
         Map m = Map.parse(Day13Test.EXAMPLE_INPUT);
         m.tick();
         assertEquals(
-                "/--A\\\n" +
+                "/--A\\        \n" +
                 "|   |  /----\\\n" +
                 "| /-+--+-\\  |\n" +
                 "| | |  | |  |\n" +
                 "\\-+-/  \\-B--/\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
         m.tick();
         assertEquals(
-                "/---A\n" +
+                "/---A        \n" +
                 "|   |  /----\\\n" +
                 "| /-+--+-\\  |\n" +
                 "| | |  | |  |\n" +
                 "\\-+-/  \\-+B-/\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
         m.tick();
         assertEquals(
-                "/---\\\n" +
+                "/---\\        \n" +
                 "|   A  /----\\\n" +
                 "| /-+--+-\\  |\n" +
                 "| | |  | |  |\n" +
                 "\\-+-/  \\-+-B/\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
         m.tick();
         assertEquals(
-                "/---\\\n" +
+                "/---\\        \n" +
                 "|   |  /----\\\n" +
                 "| /-A--+-\\  |\n" +
                 "| | |  | |  |\n" +
                 "\\-+-/  \\-+--B\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
         m.tick();
         assertEquals(
-                "/---\\\n" +
+                "/---\\        \n" +
                 "|   |  /----\\\n" +
                 "| /-+A-+-\\  |\n" +
                 "| | |  | |  B\n" +
                 "\\-+-/  \\-+--/\n" +
-                "  \\------/",
+                "  \\------/   ",
                 m.toString());
         m.tick();
         assertEquals(
-                "/---\\\n" +
+                "/---\\        \n" +
                 "|   |  /----\\\n" +
                 "| /-+-A+-\\  B\n" +
                 "| | |  | |  |\n" +
                 "\\-+-/  \\-+--/\n" +
-                "  \\------/",
+                "  \\------/   ",
+                m.toString());
+        m.tick();
+        m.tick();
+        m.tick();
+        m.tick();
+        m.tick();
+        m.tick();
+        m.tick();
+        assertEquals(
+                "/---\\        \n" +
+                "|   |  /----\\\n" +
+                "| /-+--B-\\  |\n" +
+                "| | |  | |  |\n" +
+                "\\-+-/  A-+--/\n" +
+                "  \\------/   ",
                 m.toString());
     }
 }
