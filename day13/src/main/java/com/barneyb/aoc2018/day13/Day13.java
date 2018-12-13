314eac294bac14a26819d4ac813a67094e886af9
Barney Boisvert <bboisvert@gmail.com>
2018-12-13 07:55:41 -0800

    skeleton for day 13

---
+++ day13/src/main/java/com/barneyb/aoc2018/day13/Day13.java
@@ -0,0 +1,24 @@
+package com.barneyb.aoc2018.day13;
+
+import com.barneyb.aoc2018.util.Answers;
+import com.barneyb.aoc2018.util.OneShotDay;
+import com.barneyb.aoc2018.util.FileUtils;
+
+public class Day13 extends OneShotDay {
+
+    @Override
+    public Answers solve(String input) {
+        return new Answers(
+                input.length()
+                , input.trim().length()
+        );
+    }
+
+    public static void main(String[] args)  {
+        Day13 d = new Day13();
+        String input = FileUtils.readFile("day13/input.txt");
+        Answers a = d.solve(input);
+        System.out.println(a);
+    }
+
+}
