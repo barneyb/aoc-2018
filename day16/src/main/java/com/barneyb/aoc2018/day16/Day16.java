6a10828af9dc0d3b9b36c16be58347ea4715bd5d
Barney Boisvert <bboisvert@gmail.com>
2018-12-16 09:24:49 -0800

    skeleton of the top-level algorithm

--- day16/src/main/java/com/barneyb/aoc2018/day16/Day16.java
+++ day16/src/main/java/com/barneyb/aoc2018/day16/Day16.java
@@ -1,17 +1,45 @@
 package com.barneyb.aoc2018.day16;
 
 import com.barneyb.aoc2018.util.Answers;
-import com.barneyb.aoc2018.util.OneShotDay;
 import com.barneyb.aoc2018.util.FileUtils;
+import com.barneyb.aoc2018.util.List;
+import com.barneyb.aoc2018.util.OneShotDay;
 
 public class Day16 extends OneShotDay {
 
     @Override
     public Answers solve(String input) {
+        String[] lines = input.trim().split("\n");
+        List<Sample> samples = new List<>();
+        int i = 0;
+        // first parse samples
+        while (i < lines.length) {
+            String a = lines[i++];
+            if (a.length() == 0) {
+                // end of samples
+                break;
+            }
+            samples.add(Sample.parse(a, lines[i++], lines[i++]));
+            i += 1; // the blank line
+        }
+        // this is where the sample program parser goes
         return new Answers(
-                input.length()
-                , input.trim().length()
+                partOne(samples)
+//                , input.trim().length()
         );
+    }
+
+    private int partOne(List<Sample> samples) {
+        int sampleCount = 0;
+        for (Sample s : samples) {
+            int opCount = 0;
+            // for each op
+            //    if it matches, add to opCount
+            if (opCount >= 3) {
+                sampleCount += 1;
+            }
+        }
+        return sampleCount;
     }
 
     public static void main(String[] args)  {
--- day16/src/test/java/com/barneyb/aoc2018/day16/Day16Test.java
+++ day16/src/test/java/com/barneyb/aoc2018/day16/Day16Test.java
@@ -1,20 +1,22 @@
 package com.barneyb.aoc2018.day16;
 
-import com.barneyb.aoc2018.api.Day;
 import com.barneyb.aoc2018.util.Answers;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Day16Test {
 
-    private static final String EXAMPLE_INPUT = "  cat  ";
+    static final String EXAMPLE_INPUT =
+            "Before: [3, 2, 1, 1]\n" +
+            "9 2 1 2\n" +
+            "After:  [3, 2, 2, 1]\n";
 
     @Test
     public void solve() {
         Answers a = new Day16().solve(EXAMPLE_INPUT);
-        assertEquals("7", a.getPartOne());
-        assertEquals("3", a.getPartTwo());
+        assertEquals("1", a.getPartOne());
+        assertEquals("-", a.getPartTwo());
     }
 
 }
