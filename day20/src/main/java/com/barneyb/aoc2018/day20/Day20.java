1dd6985b02a0e704e9afdee10af1a461d6f3239a
Barney Boisvert <bboisvert@gmail.com>
2018-12-20 09:28:37 -0800

    build a map from the regex

--- day20/src/main/java/com/barneyb/aoc2018/day20/Day20.java
+++ day20/src/main/java/com/barneyb/aoc2018/day20/Day20.java
@@ -2,14 +2,84 @@ package com.barneyb.aoc2018.day20;
 
 import com.barneyb.aoc2018.util.*;
 
+import static com.barneyb.aoc2018.util.Dir.*;
+
 public class Day20 extends OneShotDay {
 
     @Override
     public Answers solve(String input) {
         return new Answers(
                 input.length()
-                , input.trim().length()
+//                , input.trim().length()
         );
+    }
+
+    static int partOne(String regex) {
+        System.out.println(plotDoors(findDoors(regex)));
+        return -1;
+    }
+
+    static TreeSet<Point> findDoors(String regex) {
+        regex = regex.trim();
+        CharDigest re = new CharDigest(regex.substring(1, regex.length() - 1).toCharArray());
+        TreeSet<Point> doors = new TreeSet<>();
+        findDoors(re, doors, new Point(0, 0));
+        return doors;
+    }
+
+    static void findDoors(CharDigest re, TreeSet<Point> doors, Point start) {
+        Point p = start;
+        while (re.hasNext()) {
+            char c = re.next();
+            if (c == '(') {
+                findDoors(re, doors, p);
+            } else if (c == ')') {
+                return; // done with this subexpression
+            } else if (c == '|') {
+                p = start;
+            } else if (c == 'N') {
+                doors.add(p.go(UP));
+                p = p.go(UP, 2);
+            } else if (c == 'S') {
+                doors.add(p.go(DOWN));
+                p = p.go(DOWN, 2);
+            } else if (c == 'E') {
+                doors.add(p.go(RIGHT));
+                p = p.go(RIGHT, 2);
+            } else if (c == 'W') {
+                doors.add(p.go(LEFT));
+                p = p.go(LEFT, 2);
+            } else {
+                throw new IllegalArgumentException("what? a '" + c + "'?");
+            }
+        }
+    }
+
+    static String plotDoors(TreeSet<Point> doors) {
+        Bounds b = new Bounds(new Point(0, 0), new Point(0, 0));
+        for (Point p : doors) {
+            b = b.plus(p);
+        }
+        int minX = b.min().x - 1;
+        int minY = b.min().y - 1;
+        int maxX = b.max().x + 1;
+        int maxY = b.max().y + 1;
+        StringBuilder sb = new StringBuilder();
+        for (int y = minY; y <= maxY; y++) {
+            if (y > minY) sb.append('\n');
+            for (int x = minX; x <= maxX; x++) {
+                if (x == 0 && y == 0) {
+                    sb.append('X');
+                } else if (doors.contains(new Point(x, y))) {
+                    sb.append(x % 2 == 0 ? '-' : '|');
+                } else if (x % 2 == 0 && y % 2 == 0) {
+                    sb.append('.');
+                } else {
+                    sb.append('#');
+                }
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args)  {
--- day20/src/test/java/com/barneyb/aoc2018/day20/Day20Test.java
+++ day20/src/test/java/com/barneyb/aoc2018/day20/Day20Test.java
@@ -1,10 +1,9 @@
 package com.barneyb.aoc2018.day20;
 
-import com.barneyb.aoc2018.api.Day;
 import com.barneyb.aoc2018.util.Answers;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Day20Test {
 
@@ -14,7 +13,99 @@ public class Day20Test {
     public void solve() {
         Answers a = new Day20().solve(EXAMPLE_INPUT);
         assertEquals("7", a.getPartOne());
-        assertEquals("3", a.getPartTwo());
+        assertEquals("-", a.getPartTwo());
     }
 
+    @Test
+    public void exampleOne_a() {
+        String input = "^WNE$";
+        assertEquals(
+                "#####\n" +
+                "#.|.#\n" +
+                "#-###\n" +
+                "#.|X#\n" +
+                "#####",
+                Day20.plotDoors(Day20.findDoors(input)));
+//        assertEquals(3, Day20.partOne(input));
+    }
+
+    @Test
+    public void exampleOne_b() {
+        String input = "^ENWWW(NEEE|SSE(EE|N))$";
+        assertEquals(
+                "#########\n" +
+                "#.|.|.|.#\n" +
+                "#-#######\n" +
+                "#.|.|.|.#\n" +
+                "#-#####-#\n" +
+                "#.#.#X|.#\n" +
+                "#-#-#####\n" +
+                "#.|.|.|.#\n" +
+                "#########",
+                Day20.plotDoors(Day20.findDoors(input)));
+//        assertEquals(10, Day20.partOne(input));
+    }
+
+    @Test
+    public void exampleOne_c() {
+        String input = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
+        assertEquals(
+                "###########\n" +
+                "#.|.#.|.#.#\n" +
+                "#-###-#-#-#\n" +
+                "#.|.|.#.#.#\n" +
+                "#-#####-#-#\n" +
+                "#.#.#X|.#.#\n" +
+                "#-#-#####-#\n" +
+                "#.#.|.|.|.#\n" +
+                "#-###-###-#\n" +
+                "#.|.|.#.|.#\n" +
+                "###########",
+                Day20.plotDoors(Day20.findDoors(input)));
+//        assertEquals(18, Day20.partOne(input));
+    }
+
+    @Test
+    public void exampleOne_d() {
+        String input = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
+        assertEquals(
+                "#############\n" +
+                "#.|.|.|.|.|.#\n" +
+                "#-#####-###-#\n" +
+                "#.#.|.#.#.#.#\n" +
+                "#-#-###-#-#-#\n" +
+                "#.#.#.|.#.|.#\n" +
+                "#-#-#-#####-#\n" +
+                "#.#.#.#X|.#.#\n" +
+                "#-#-#-###-#-#\n" +
+                "#.|.#.|.#.#.#\n" +
+                "###-#-###-#-#\n" +
+                "#.|.#.|.|.#.#\n" +
+                "#############",
+                Day20.plotDoors(Day20.findDoors(input)));
+//        assertEquals(23, Day20.partOne(input));
+    }
+
+    @Test
+    public void exampleOne_e() {
+        String input = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
+        assertEquals(
+                "###############\n" +
+                "#.|.|.|.#.|.|.#\n" +
+                "#-###-###-#-#-#\n" +
+                "#.|.#.|.|.#.#.#\n" +
+                "#-#########-#-#\n" +
+                "#.#.|.|.|.|.#.#\n" +
+                "#-#-#########-#\n" +
+                "#.#.#.|X#.|.#.#\n" +
+                "###-#-###-#-#-#\n" +
+                "#.|.#.#.|.#.|.#\n" +
+                "#-###-#####-###\n" +
+                "#.|.#.|.|.#.#.#\n" +
+                "#-#-#####-#-#-#\n" +
+                "#.#.|.|.|.#.|.#\n" +
+                "###############",
+                Day20.plotDoors(Day20.findDoors(input)));
+//        assertEquals(31, Day20.partOne(input));
+    }
 }
