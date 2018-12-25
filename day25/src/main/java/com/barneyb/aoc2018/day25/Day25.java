fc13c2f564489b8f0f2e19dd95a7d6dcc19a45c8
Barney Boisvert <bboisvert@gmail.com>
2018-12-25 13:12:50 -0800

    day 25 part one

--- day25/src/main/java/com/barneyb/aoc2018/day25/Day25.java
+++ day25/src/main/java/com/barneyb/aoc2018/day25/Day25.java
@@ -6,9 +6,25 @@ public class Day25 extends OneShotDay {
 
     @Override
     public Answers solve(String input) {
+        String[] lines = input.trim().split("\n");
+        Vector[] points = new Vector[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            points[i] = Vector.parse(lines[i]);
+        }
+        UF uf = new UF(points.length);
+        for (int i = 0; i < points.length; i++) {
+            Vector p = points[i];
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) continue;
+                Vector q = points[j];
+                if (p.md(q) <= 3) {
+                    uf.union(i, j);
+                }
+            }
+        }
         return new Answers(
-                input.length()
-                , input.trim().length()
+                uf.components()
+//                , input.trim().length()
         );
     }
 
