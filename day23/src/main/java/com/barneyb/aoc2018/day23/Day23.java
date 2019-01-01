c834a9d0016d63a7036579c0daf63a87c6d3ee5c
Barney Boisvert <bboisvert@gmail.com>
2018-12-31 19:13:19 -0800

    strip out a bunch of debugging

--- day23/src/main/java/com/barneyb/aoc2018/day23/Day23.java
+++ day23/src/main/java/com/barneyb/aoc2018/day23/Day23.java
@@ -120,20 +120,6 @@ public class Day23 extends OneShotDay {
         // in terms of the two bots who create it, within the set of all bots
         // that are in signal range of the solution points
 
-        Vector best = bests.iterator().next();
-        int bestS = best.dim(0), bestI = best.dim(1);
-
-        Bot a = bots[bestS];
-        System.out.println(a);
-        Bot b = bots[bestI];
-        System.out.println(b);
-        System.out.println("  overlap: " + a.overlapsBy(b));
-        Vector mid = a.midpoint(b);
-        System.out.println(" midpoint: " + mid);
-        System.out.println("     in a: " + a.inRange(mid) + " (" + a.pos.md(mid) + ")");
-        System.out.println("     in b: " + b.inRange(mid) + " (" + b.pos.md(mid) + ")");
-        System.out.println(" distance: " + mid.md(ORIGIN));
-
         return partTwo_b(swarm, allSites, bests);
     }
 
@@ -168,25 +154,16 @@ public class Day23 extends OneShotDay {
         Bot a, b;
         TreeSet<Vector> intersect = null;
 
-        System.out.println("Maxed:");
         a = bots[maxPair.dim(0)];
-        System.out.println(a);
         b = bots[maxPair.dim(1)];
-        System.out.println(b);
         Vector start = a.midpoint(b);
 
         // grab "about 1/100th of the range", exactly a power of ten
-        int initialStep = (int) Math.pow(10, (int) Math.log10(a.range / 100.0));
+        int initialStep = (int) Math.pow(10, (int) Math.log10(Math.max(a.range, b.range) / 100.0));
         for (int s = initialStep; s >= 1; s /= 10) {
-            System.out.printf("Using pitch %,d%n", s);
+            System.out.printf("Using pitch %,d...%n", s);
             intersect = intersection(start, bots, s);
             start = intersect.min();
-            System.out.printf("w/ pitch %,d, intersection: %d points:%n", s, intersect.size());
-            int cutoff = 0;
-            for (Vector p : intersect) {
-                System.out.printf("  %s : %,d%n", p, ORIGIN.md(p));
-                if (++cutoff == 20) break;
-            }
         }
         assert intersect != null;
         Vector[] arr = new Vector[intersect.size()];
@@ -221,7 +198,6 @@ public class Day23 extends OneShotDay {
             if (count < bestCount - 2) continue; // leave two (not one), because of the 3-d manhattan distance wobble
             if (count > bestCount) {
                 bestCount = count;
-                System.out.printf("new best: %d (of %d), old points: %,d%n", bestCount, bots.length, points.size());
                 points.clear();
             }
             if (count == bestCount) {
@@ -237,8 +213,6 @@ public class Day23 extends OneShotDay {
         System.out.printf("queued: %,d, best: %d (of %d), points: %,d, visited: %,d%n", queue.size(), bestCount, bots.length, points.size(), visited.size());
         return points;
     }
-
-    // 138,697,281
 
     public static void main(String[] args)  {
         Day23 d = new Day23();
