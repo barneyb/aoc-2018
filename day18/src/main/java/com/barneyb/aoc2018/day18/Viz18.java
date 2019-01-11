ae473cd2176c270513eb628b0be21de983b585c7
Barney Boisvert <bboisvert@gmail.com>
2019-01-11 13:57:45 -0800

    slightly more efficient drawing

--- day18/src/main/java/com/barneyb/aoc2018/day18/Viz18.java
+++ day18/src/main/java/com/barneyb/aoc2018/day18/Viz18.java
@@ -13,37 +13,42 @@ public class Viz18 {
     static final Color C_TREES = Color.GREEN;
     static final Color C_LUMBERYARD = Color.DARK_GRAY;
 
-    public static void main(String[] args) {
-        String input = FileUtils.readFile("day18/input.txt");
-        Map m = Map.parse(input);
-        Scene s = new Scene();
-        s.addFrame(toFrame(m));
-        // just do a thousand ticks
-        for (int i = 0; i < 500; i++) {
-            m.tick();
-            s.addFrame(toFrame(m));
+    private final Scene scene;
+    private final int width;
+    private final int height;
+    private final int pitch;
+
+    Viz18(Map map) {
+        width = map.width();
+        height = map.height();
+        pitch = 1000 / Math.max(width, height);
+        scene = new Scene(new Frame(
+                color(C_OPEN),
+                Region.rect(0, 0, pitch * width, pitch * height)));
+        scene.addFrame(toFrame(map));
+        for (int i = 0; i < 300; i++) {
+            map.tick();
+            scene.addFrame(toFrame(map));
         }
-        new SceneView().view(s);
     }
 
-    private static Frame toFrame(Map m) {
-        int width = m.width();
-        int height = m.height();
-        int pitch = 500 / Math.max(width, height);
+    void view() {
+        new SceneView().view(scene);
+    }
+
+    private Frame toFrame(Map m) {
         Bag<Region> lumberyards = new Bag<>();
         Bag<Region> trees = new Bag<>();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 char c = m.curr(x, y);
+                if (c == Map.OPEN) continue;
                 Region r = Region.rect(x * pitch, y * pitch, pitch, pitch);
                 if (c == Map.LUMBERYARD) lumberyards.add(r);
                 else if (c == Map.TREES) trees.add(r);
             }
         }
-        Frame f = new Frame(
-                color(C_OPEN),
-                Region.rect(0, 0, pitch * width, pitch * height)
-        );
+        Frame f = new Frame();
         f.addElement(new Element(color(C_LUMBERYARD)));
         for (Region r : lumberyards) {
             f.addElement(new Element(r));
@@ -55,9 +60,15 @@ public class Viz18 {
         return f;
     }
 
-    private static Command color(Color color) {
+    private Command color(Color color) {
         float[] comp = color.getColorComponents(null);
         return Command.color(comp[0], comp[1], comp[2]);
+    }
+
+    public static void main(String[] args) {
+        new Viz18(Map.parse(
+                FileUtils.readFile("day18/input.txt")
+        )).view();
     }
 
 }
