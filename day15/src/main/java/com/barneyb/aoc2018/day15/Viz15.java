d7cff901153c991ec1d7c1e135c8805bd996628c
Barney Boisvert <bboisvert@gmail.com>
2019-01-12 12:18:11 -0800

    draw the goblins and elves too!

--- day15/src/main/java/com/barneyb/aoc2018/day15/Viz15.java
+++ day15/src/main/java/com/barneyb/aoc2018/day15/Viz15.java
@@ -7,12 +7,16 @@ import com.barneyb.jpixelclient.raw.SceneView;
 
 import java.awt.*;
 
+import static com.barneyb.aoc2018.util.VizUtils.awt;
+
 public class Viz15 {
 
     static final Color C_FLOOR = Color.LIGHT_GRAY;
     static final Color C_WALL = Color.DARK_GRAY;
+    static final Color C_ELF = new Color(32, 162, 0);
+    static final Color C_GOBLIN = new Color(186, 108, 102);
 
-    private Raster cavern;
+    private final Scene scene;
     private final int width;
     private final int height;
     private final int pitch;
@@ -21,18 +25,23 @@ public class Viz15 {
         width = map.width();
         height = map.height();
         pitch = 1000 / Math.max(width, height);
-        cavern = new Raster();
+        Raster cavern = new Raster();
         cavern.fill(new Rectangle(0, 0, width, height), C_FLOOR);
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 if (map.isWall(x, y)) cavern.dot(x, y, C_WALL);
             }
         }
+        scene = new Scene(cavern.asFrame(pitch));
+        Raster units = new Raster();
+        for (Unit u : map.survivors()) {
+            units.dot(awt(u.location()), u.isElf() ? C_ELF : C_GOBLIN);
+        }
+        scene.addFrame(units.asFrame(pitch));
     }
 
     public void view() {
-        Scene s = new Scene(cavern.asFrame(pitch));
-        new SceneView().view(s);
+        new SceneView().view(scene);
     }
 
     public static void main(String[] args) {
